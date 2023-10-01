d551c9513c4fb59da7f317b16a4444b6db93b1a8
psevdo212 <psevdo-2002@yandex.ru>
2023-10-01 11:36:36 +0300

    pm2

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,23 @@
+require('dotenv').config({ path: './.env.deploy' });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/main', DEPLOY_REPO,
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'frontend',
+  }],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'pre-deploy-local': `scp ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': `cd ${DEPLOY_PATH}/source/frontend && npm i && npm run build`,
+    },
+  },
+};
